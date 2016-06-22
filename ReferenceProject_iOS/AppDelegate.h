c83335ea80d7143e0d6f81801dac1464ed57d2c3
saurabh <saurabhs@vmokshagroup.com>
2016-06-22 15:35:00 +0530

    Initial Commit

---
+++ ReferenceProject_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReferenceProject_iOS
+//
+//  Created by Saurabh on 6/22/16.
+//  Copyright © 2016 vmoksha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReferenceProject_iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReferenceProject_iOS
+//
+//  Created by Saurabh on 6/22/16.
+//  Copyright © 2016 vmoksha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
