962565b7926f60eaab9c8eecdec92add8fd4e5f1
ManasaMP <manasap@vmokshagroup.com>
2016-06-23 16:06:28 +0530

    commit after theme change

--- ReferenceProject_iOS/LocalizeViewController.m
+++ ReferenceProject_iOS/LocalizeViewController.m
@@ -1,21 +1,56 @@
 #import "LocalizeViewController.h"
+@interface LocalizeViewController ()<UITableViewDelegate,UITableViewDataSource>
 
-@interface LocalizeViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *dataLabel;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *tableviewHeight;
 @end
 
 @implementation LocalizeViewController
-
+{
+    NSIndexPath *selectedIndex;
+    int numberOfRows;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationController.navigationBarHidden=NO;
       self.navigationItem.hidesBackButton=NO;
+    numberOfRows=0;
+    [self readDataFromFile:@"English"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
    
 }
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return numberOfRows;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"cell"];
+    UILabel *label=(UILabel*)[cell viewWithTag:10];
+   
+    tableView.tableFooterView=[UIView new];
+    if ([selectedIndex isEqual:indexPath]) {
+        cell.backgroundColor=[UIColor lightGrayColor];
+    }else cell.backgroundColor=[UIColor whiteColor];
+       return cell;
+}
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
 
-
-
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 30;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 10;
+}
+-(void)readDataFromFile:(NSString*)languageCode{
+    NSMutableDictionary *jsonDictionary=[[NSMutableDictionary alloc]init];
+    NSString *filepath=[[NSBundle mainBundle]pathForResource:languageCode ofType:@".json"];
+    NSURL *fileUrl=[NSURL fileURLWithPath:filepath];
+    NSString *name=[filepath stringByDeletingPathExtension];
+    [jsonDictionary setObject:fileUrl forKey:name];
+    numberOfRows=3;
+}
 @end
--- ThemeViewController.m
+++ ThemeViewController.m
@@ -1,5 +1,5 @@
 //Drag Drop the "Them Images" into the "Assets.xcassets".
-//Create a array which have list of images.
+//Create a array which have list of images if we use iimage as background or color name.
 //use tableview to display the image list.
 // select any image and show that image as background image.
 //store selected image in "NSUserDefault".
@@ -12,6 +12,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *backGroundImageView;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *tableViewHeight;
 @property (weak, nonatomic) IBOutlet UITableView *themeTableView;
+@property (weak, nonatomic) IBOutlet UILabel *selectThemeLabel;
 
 @end
 
@@ -19,20 +20,29 @@
 {
     NSArray *themeValueArray;
     NSUserDefaults *defaultValue;
+    NSIndexPath *selectedIndex;
+    UIColor *selectedColor;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationController.navigationBarHidden=NO;
     self.navigationItem.hidesBackButton=NO;
-    themeValueArray=@[@"background",@"nature",@"water",@"red"];
+    //themeValueArray=@[@"background",@"nature",@"water",@"red"]; // For Image Background
+    themeValueArray=@[@"Red",@"Blue",@"Green"];
     defaultValue=[NSUserDefaults standardUserDefaults];
     NSString *backGroundImageName=[defaultValue valueForKey:@"backGroundImage"];
     if (backGroundImageName!=nil) {
-        _backGroundImageView.image=[UIImage imageNamed:backGroundImageName];
-        [defaultValue setValue:backGroundImageName forKey:@"backGroundImage"];
+        //_backGroundImageView.image=[UIImage imageNamed:backGroundImageName];  // For Image Background
+        _backGroundImageView.hidden=YES;
+        int i=(int)[themeValueArray indexOfObject:backGroundImageName];
+        [self getTheColor:i];
+        selectedIndex=[NSIndexPath indexPathForRow:0 inSection:i];
     }
     else{
-        backGroundImageName=@"background";
-    [defaultValue setValue:backGroundImageName forKey:@"backGroundImage"];
+        /*[defaultValue setValue:@"background" forKey:@"backGroundImage"];
+        _backGroundImageView.image=[UIImage imageNamed:backGroundImageName];*/ // For Image Background
+        _backGroundImageView.hidden=NO;
+        selectedColor=[UIColor blackColor];
     }
 }
 
@@ -48,10 +58,15 @@
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"cell"];
-    UIImageView *imageView=(UIImageView*)[cell viewWithTag:10];
-    imageView.image=[UIImage imageNamed:themeValueArray[indexPath.section]];
+    UILabel *label=(UILabel*)[cell viewWithTag:10];
+//    imageView.image=[UIImage imageNamed:themeValueArray[indexPath.section]]; // For Image Background
+    label.text=themeValueArray[indexPath.section];
     tableView.tableFooterView=[UIView new];
-     _tableViewHeight.constant=_themeTableView.contentSize.height;
+    label.textColor= selectedColor;
+    if ([selectedIndex isEqual:indexPath]) {
+        cell.backgroundColor=[UIColor lightGrayColor];
+    }else cell.backgroundColor=[UIColor whiteColor];
+    _tableViewHeight.constant=_themeTableView.contentSize.height;
     return cell;
 }
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -61,7 +76,38 @@
     return 10;
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
- _backGroundImageView.image=[UIImage imageNamed:themeValueArray[indexPath.section]];
+// _backGroundImageView.image=[UIImage imageNamed:themeValueArray[indexPath.section]];
+    [self getTheColor:(int)indexPath.section];
+    selectedIndex=indexPath;
      [defaultValue setValue:themeValueArray[indexPath.section] forKey:@"backGroundImage"];
+    [_themeTableView reloadData];
+}
+-(void)getTheColor:(int)value{
+      _backGroundImageView.hidden=YES;
+    switch (value) {
+        case 0:
+            self.view.backgroundColor=[UIColor redColor];
+            [self.navigationController.navigationBar setTintColor:[UIColor redColor]];
+            _selectThemeLabel.textColor=[UIColor redColor];
+            selectedColor =[UIColor redColor];
+            _themeTableView.backgroundColor=[UIColor redColor];
+            break;
+        case 1:
+            self.view.backgroundColor=[UIColor blueColor];
+            [self.navigationController.navigationBar setTintColor:[UIColor blueColor]];
+            _selectThemeLabel.textColor=[UIColor blueColor];
+             selectedColor =[UIColor blueColor];
+            _themeTableView.backgroundColor=[UIColor blueColor];
+            break;
+        case 2:
+            self.view.backgroundColor=[UIColor greenColor];
+            [self.navigationController.navigationBar setTintColor:[UIColor greenColor]];
+            _selectThemeLabel.textColor=[UIColor greenColor];
+             selectedColor =[UIColor greenColor];
+            _themeTableView.backgroundColor=[UIColor greenColor];
+            break;
+        default:
+            break;
+    }
 }
 @end
