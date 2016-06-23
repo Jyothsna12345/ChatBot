392d1b9fc88ba8d227d83bcb815ded6a39ea9be5
saurabh <saurabhs@vmokshagroup.com>
2016-06-23 11:37:30 +0530

    commit after adding class

---
+++ ReferenceProject_iOS/ChangePasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChangePasswordViewController.h
+//  ReferenceProject_iOS
+//
+//  Created by Saurabh on 6/23/16.
+//  Copyright © 2016 vmoksha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChangePasswordViewController : UIViewController
+
+@end
---
+++ ReferenceProject_iOS/ChangePasswordViewController.m
@@ -0,0 +1,37 @@
+//
+//  ChangePasswordViewController.m
+//  ReferenceProject_iOS
+//
+//  Created by Saurabh on 6/23/16.
+//  Copyright © 2016 vmoksha. All rights reserved.
+//
+
+#import "ChangePasswordViewController.h"
+
+@interface ChangePasswordViewController ()
+
+@end
+
+@implementation ChangePasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
